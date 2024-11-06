40244260d36482388c3ed537910c30b2f746dd11
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-11-06 15:22:22 -0300

    sobrecarga de funciones

--- TPINT_GRUPO_8_PR3/DAO/AccesoDatos.cs
+++ TPINT_GRUPO_8_PR3/DAO/AccesoDatos.cs
@@ -30,19 +30,6 @@ namespace DAO
                 return null;
             }
         }
-        private SqlDataAdapter ObtenerAdaptador(String consultaSql, SqlConnection cn)
-        {
-            SqlDataAdapter adaptador;
-            try
-            {
-                adaptador = new SqlDataAdapter(consultaSql, cn);
-                return adaptador;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
         private SqlCommand ObtenerComando(string consultaSql, SqlConnection cn, Dictionary<string, object> parametros = null)
         {
             SqlCommand comando = new SqlCommand(consultaSql, cn);
@@ -60,6 +47,32 @@ namespace DAO
                 return null;
             }
         }
+        private SqlDataAdapter ObtenerAdaptador(String consultaSql, SqlConnection cn)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(consultaSql, cn);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        private SqlDataAdapter ObtenerAdaptador(SqlCommand cmd)
+        {
+            SqlDataAdapter adaptador;
+            try
+            {
+                adaptador = new SqlDataAdapter(cmd);
+                return adaptador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
         public DataTable ObtenerTabla(String NombreTabla, String Sql)
         {
             DataSet ds = new DataSet();
@@ -68,6 +81,49 @@ namespace DAO
             adp.Fill(ds, NombreTabla);
             con.Close();
             return ds.Tables[NombreTabla];
+        }
+        public DataTable ObtenerTabla(string NombreTabla, String Sql, Dictionary<string, object> parametros)
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = ObtenerConexion();
+            SqlCommand cmd = ObtenerComando(Sql, con, parametros);
+            SqlDataAdapter adap = ObtenerAdaptador(cmd);
+            adap.Fill(ds, NombreTabla);
+            con.Close();
+            return ds.Tables[NombreTabla];
+        }
+        public int InsertarRegistro(string NombreTabla, String sql)
+        {
+            DataSet ds = new DataSet();
+            int filas = 0;
+            SqlConnection con = ObtenerConexion();
+            SqlCommand cmd = ObtenerComando(sql, con);
+            filas = cmd.ExecuteNonQuery();
+            con.Close();
+            return filas;
+        }
+        public int ModificarRegistro(string NombreTabla, String sql, Dictionary<string, object> parametros)
+        {
+            DataSet ds = new DataSet();
+            int filas = 0;
+            SqlConnection con = ObtenerConexion();
+            SqlCommand cmd = ObtenerComando(sql, con, parametros);
+            filas = cmd.ExecuteNonQuery();
+            con.Close();
+            return filas;
+        }
+        public int EjecutarProcedimientoAlmacenado(SqlCommand Comando, String NombreSP)
+        {
+            int FilasCambiadas;
+            SqlConnection Conexion = ObtenerConexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd = Comando;
+            cmd.Connection = Conexion;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = NombreSP;
+            FilasCambiadas = cmd.ExecuteNonQuery();
+            Conexion.Close();
+            return FilasCambiadas;
         }
         public Boolean existe(String consulta)
         {
